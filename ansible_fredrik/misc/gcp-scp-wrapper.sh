74dc7eac3239c56b0f9931625fd2c0914ad74ada
Fredrik Magnusson <fredrikmagnusson3@gmail.com>
2023-09-11 01:59:23 +0200

    test

--- ansible_fredrik/misc/gcp-scp-wrapper.sh
+++ ansible_fredrik/misc/gcp-scp-wrapper.sh
@@ -6,19 +6,19 @@
 # second to last argument and the command as the last. We will pop the last two
 # arguments off of the list and then pass all of the other SSH flags through
 # without modification:
-host="${@: -2: 1}"
-cmd="${@: -1: 1}"
+host="${@: -2:1}"
+cmd="${@: -1:1}"
 
 # Unfortunately ansible has hardcoded scp options, so we need to filter these out
 # It's an ugly hack, but for now we'll only accept the options starting with '--'
 declare -a opts
-for scp_arg in "${@: 1: $# -3}" ; do
-        if [[ "${scp_arg}" == --* ]] ; then
+for scp_arg in "${@:1:$#-3}"; do
+        if [[ "${scp_arg}" == --* ]]; then
                 opts+="${scp_arg} "
         fi
 done
 
 # Remove [] around our host, as gcloud scp doesn't understand this syntax
-cmd=`echo "${cmd}" | tr -d []`
+cmd=$(echo "${cmd}" | tr -d [])
 
 exec gcloud compute scp $opts "${host}" "${cmd}"
