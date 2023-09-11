456881163df4ed478f83c84cfd2bf8770815f10c
SamanPetfat <Saman.p@hotmail.se>
2023-09-11 12:06:01 +0200

    smtp script modified

--- smtp.sh
+++ smtp.sh
@@ -15,6 +15,14 @@ date=$(date)
 hostname=$(hostname)
 summary="CI/CD PIPELINE HAVE FAILED AT $date ON $hostname."
 
+GITHUB_REPO="SamanPetfat/Gear5"
+GITHUB_TOKEN="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"
+
+PR_TITLE="Automated PR: Tests Passed"
+PR_BODY="This pull request is automatically created because all tests passed."
+PR_BASE_BRANCH="main"  # Change this to your target branch
+PR_HEAD_BRANCH="staging"  # Change this to your feature branch
+
 send_mail() {
     swaks -S --to $RECIEVER_EMAIL_ADDRESS \
              --from $SENDER_EMAIL_ADDRESS \
@@ -37,5 +45,25 @@ if [ $command_status -ne 0 ]; then
     fi
     send_mail "$body_content"
 else
-    echo "success"
+
+    PR_JSON=$(cat <<EOF
+{
+"title": "$PR_TITLE",
+"body": "$PR_BODY",
+"head": "$PR_HEAD_BRANCH",
+"base": "$PR_BASE_BRANCH"
+}
+EOF
+    )
+
+    # Create the pull request using GitHub API
+    PR_URL="https://api.github.com/repos/$GITHUB_REPO/pulls"
+    PR_RESPONSE=$(curl -X POST -H "Authorization: token $GITHUB_TOKEN" -d "$PR_JSON" "$PR_URL")
+
+    # Check the response from GitHub API
+    if [ "$(echo "$PR_RESPONSE" | jq -r '.html_url')" != "null" ]; then
+        echo "Pull request created successfully: $(echo "$PR_RESPONSE" | jq -r '.html_url')"
+    else
+        echo "Failed to create a pull request."
+    fi
 fi
