d385f9ac75bcacf03e77fd03146c79c7270c1d99
Fredrik Magnusson <fredrikmagnusson3@gmail.com>
2023-09-11 17:56:37 +0200

    test

--- smtp.sh
+++ smtp.sh
@@ -6,7 +6,7 @@ SMTP_SERVER=""
 EMAIL_ADDRESS=""
 PASSWORD=""
 
-output=$(TO <COMMAND RUN >2 >&1)
+output=$(sleep 20 2>&1)
 command_status=$?
 output_file="/tmp/$(hostname)_output.log"
 echo "$output" >"$output_file"
@@ -14,6 +14,14 @@ num_lines=$(echo "$output" | wc -l)
 date=$(date)
 hostname=$(hostname)
 summary="CI/CD PIPELINE HAVE FAILED AT $date ON $hostname."
+
+GITHUB_REPO="SamanPetfat/Gear5"
+#GITHUB_TOKEN="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"
+
+PR_TITLE="Automated PR: Tests Passed"
+PR_BODY="This pull request is automatically created because all tests passed."
+PR_BASE_BRANCH="main"    # Change this to your target branch
+PR_HEAD_BRANCH="staging" # Change this to your feature branch
 
 send_mail() {
     swaks -S --to $RECIEVER_EMAIL_ADDRESS \
@@ -30,12 +38,33 @@ send_mail() {
 
 if [ $command_status -ne 0 ]; then
     if [ $num_lines -le 25 ]; then
-        body_content="$output\n\nPlease check the server immediately!"
+        body_content="$output\n\nPlease check the code immediately!"
     else
         last_50_lines=$(echo "$output" | tail -n 50)
-        body_content="LAST 50 LINES FROM THE LOG FILE:\n\n$last_50_lines\n\nSee attached log file for more.\n\nPlease check the server immediately!"
+        body_content="$last_50_lines\n\nSee attached log file for more.\n\nPlease check the code immediately!"
     fi
     send_mail "$body_content"
 else
-    echo "success"
+
+    PR_JSON=$(
+        cat <<EOF
+{
+"title": "$PR_TITLE",
+"body": "$PR_BODY",
+"head": "$PR_HEAD_BRANCH",
+"base": "$PR_BASE_BRANCH"
+}
+EOF
+    )
+
+    # Create the pull request using GitHub API
+    PR_URL="https://api.github.com/repos/$GITHUB_REPO/pulls"
+    PR_RESPONSE=$(curl -X POST -H "Authorization: token $GITHUB_TOKEN" -d "$PR_JSON" "$PR_URL")
+
+    # Check the response from GitHub API
+    if [ "$(echo "$PR_RESPONSE" | jq -r '.html_url')" != "null" ]; then
+        echo "Pull request created successfully: $(echo "$PR_RESPONSE" | jq -r '.html_url')"
+    else
+        echo "Failed to create a pull request."
+    fi
 fi
