4a55704275b8cbdf22e37ae80436e60e99adae3f
SamanPetfat <SamanPetfat@users.noreply.github.com>
2023-09-14 17:16:02 +0000

    Apply PHPCBF fixes

--- www/html/wp-content/themes/bizboost/assets/js/custom.js
+++ www/html/wp-content/themes/bizboost/assets/js/custom.js
@@ -7,12 +7,12 @@
  */
 let url = window.location.href;
 const catLink = document.querySelectorAll(
-  ".wc-block-product-categories-list li a"
+    ".wc-block-product-categories-list li a"
 );
 catLink.forEach((item) => {
-  if (item.href === url) {
-    item.classList.add("active");
-  }
+    if (item.href === url) {
+        item.classList.add("active");
+    }
 });
 
 /*
@@ -21,32 +21,32 @@ catLink.forEach((item) => {
 let searchBtn = document.querySelector(".search-controller svg.search");
 
 if (searchBtn !== null) {
-  searchBtn.addEventListener("click", function (e) {
-    document.body.classList.remove("open-social");
-    document.body.classList.add("open-search");
-    document.body.addEventListener("click", function () {
-      document.body.classList.remove("open-search");
-    });
+    searchBtn.addEventListener("click", function (e) {
+        document.body.classList.remove("open-social");
+        document.body.classList.add("open-search");
+        document.body.addEventListener("click", function () {
+            document.body.classList.remove("open-search");
+        });
 
-    let searchContainer = document.querySelector(".search-container");
-    searchContainer.addEventListener("click", function (e) {
-      e.stopPropagation();
-    });
+        let searchContainer = document.querySelector(".search-container");
+        searchContainer.addEventListener("click", function (e) {
+            e.stopPropagation();
+        });
 
-    var searchInput = document.querySelector(".wp-block-search__input");
-    window.setTimeout(() => searchInput.focus(), 0);
-  });
+        var searchInput = document.querySelector(".wp-block-search__input");
+        window.setTimeout(() => searchInput.focus(), 0);
+    });
 }
 
 var searchBtnClose = document.querySelector(
-  ".search-controller svg.cross"
+    ".search-controller svg.cross"
 );
 
 if (searchBtnClose !== null) {
-  searchBtnClose.addEventListener("click", function (e) {
-    document.body.classList.remove("open-search");
+    searchBtnClose.addEventListener("click", function (e) {
+        document.body.classList.remove("open-search");
 
-  });
+    });
 }
 
 
@@ -56,32 +56,32 @@ if (searchBtnClose !== null) {
 let socialBtn = document.querySelector(".social-controller svg.social");
 
 if (socialBtn !== null) {
-  socialBtn.addEventListener("click", function (e) {
-    document.body.classList.remove("open-search");
-    document.body.classList.add("open-social");
-    document.body.addEventListener("click", function () {
-      document.body.classList.remove("open-social");
-    });
+    socialBtn.addEventListener("click", function (e) {
+        document.body.classList.remove("open-search");
+        document.body.classList.add("open-social");
+        document.body.addEventListener("click", function () {
+            document.body.classList.remove("open-social");
+        });
 
-    let socialContainer = document.querySelector(".social-container");
-    socialContainer.addEventListener("click", function (e) {
-      e.stopPropagation();
-    });
+        let socialContainer = document.querySelector(".social-container");
+        socialContainer.addEventListener("click", function (e) {
+            e.stopPropagation();
+        });
 
-    var socialInput = document.querySelector(".wp-block-social__input");
-    window.setTimeout(() => socialInput.focus(), 0);
-  });
+        var socialInput = document.querySelector(".wp-block-social__input");
+        window.setTimeout(() => socialInput.focus(), 0);
+    });
 }
 
 var socialBtnClose = document.querySelector(
-  ".social-controller svg.cross"
+    ".social-controller svg.cross"
 );
 
 if (socialBtnClose !== null) {
-  socialBtnClose.addEventListener("click", function (e) {
-    document.body.classList.remove("open-social");
+    socialBtnClose.addEventListener("click", function (e) {
+        document.body.classList.remove("open-social");
 
-  });
+    });
 }
 
 /*
@@ -90,10 +90,10 @@ if (socialBtnClose !== null) {
 let blinkerField = document.querySelector(".social-controller svg.search");
 
 if (blinkerField !== null) {
-  blinkerField.addEventListener("click", function () {
-    var searchInput = document.querySelector(".wp-block-search__input");
+    blinkerField.addEventListener("click", function () {
+        var searchInput = document.querySelector(".wp-block-search__input");
         window.setTimeout(() => searchInput.focus(), 0);
-  });
+    });
 }
 
 // Moving Object With Cursor
@@ -108,35 +108,36 @@ let ballY = 0;
 
 let speed = 0.2;  //how fast ball catches up to mouse pointer;
 
-function animate() {
-  let distX = mouseX - ballX;
-  let distY = mouseY - ballY;
+function animate()
+{
+    let distX = mouseX - ballX;
+    let distY = mouseY - ballY;
 
-  ballX = ballX + (distX * speed);
-  ballY = ballY + (distY * speed);
+    ballX = ballX + (distX * speed);
+    ballY = ballY + (distY * speed);
 
-  bob.style.left = ballX + 'px';
-  bob.style.top = ballY + 'px';
+    bob.style.left = ballX + 'px';
+    bob.style.top = ballY + 'px';
 
-  requestAnimationFrame(animate)
+    requestAnimationFrame(animate)
 
 };
 
 animate();
 
 document.addEventListener('mousemove', function (e) {
-  mouseX = e.pageX;
-  mouseY = e.pageY;
+    mouseX = e.pageX;
+    mouseY = e.pageY;
 });
 
 document.addEventListener('click', function (e) {
-  e.preventDefault;
-  bob.classList.remove('active');
+    e.preventDefault;
+    bob.classList.remove('active');
   //some rando comment
 
-  void bob.offsetWidth;
+    void bob.offsetWidth;
 
-  bob.classList.add('active');
+    bob.classList.add('active');
 
 }, false);
 
@@ -152,18 +153,20 @@ let menuLinks = document.getElementsByClassName("wp-block-navigation-item__conte
 // the actual element being touched, instead of all
 // elements that are stored in variable
 for (var i = 0; i < menuLinks.length; i++) {
-  menuLinks[i].addEventListener("mouseover", menuHoverOn);
-  menuLinks[i].addEventListener("mouseout", menuHoverOff);
+    menuLinks[i].addEventListener("mouseover", menuHoverOn);
+    menuLinks[i].addEventListener("mouseout", menuHoverOff);
 }
 
 // functions to add and remove menu__active class for changing cursor styles
 let menuCursor = document.getElementById("custom-cursor");
 
-function menuHoverOn() {
-  menuCursor.classList.add("menu__active")
+function menuHoverOn()
+{
+    menuCursor.classList.add("menu__active")
 }
-function menuHoverOff() {
-  menuCursor.classList.remove("menu__active")
+function menuHoverOff()
+{
+    menuCursor.classList.remove("menu__active")
 }
 
 
@@ -177,8 +180,8 @@ let teamLinks = document.getElementsByClassName("wp-block-post-group");
 // the actual element being touched, instead of all
 // elements that are stored in variable
 for (var i = 0; i < teamLinks.length; i++) {
-  teamLinks[i].addEventListener("mouseover", teamHoverOn);
-  teamLinks[i].addEventListener("mouseout", teamHoverOff);
+    teamLinks[i].addEventListener("mouseover", teamHoverOn);
+    teamLinks[i].addEventListener("mouseout", teamHoverOff);
 }
 
 // functions to add and remove menu__active class for changing cursor styles
@@ -186,11 +189,13 @@ let teamCursor = document.getElementById("custom-cursor");
 
 let teamButton = document.getElementsByClassName(".wp-block-buttons");
 
-function teamHoverOn() {
-  teamCursor.classList.add("team__active")
+function teamHoverOn()
+{
+    teamCursor.classList.add("team__active")
 }
-function teamHoverOff() {
-  teamCursor.classList.remove("team__active")
+function teamHoverOff()
+{
+    teamCursor.classList.remove("team__active")
 }
 
 
--- www/html/wp-content/themes/bizboost/functions.php
+++ www/html/wp-content/themes/bizboost/functions.php
@@ -8,81 +8,83 @@
  * @since 1.0
  */
 
-if ( ! function_exists( 'bizboost_support' ) ) :
+if (! function_exists('bizboost_support')) :
 
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * @since 1.0
-	 *
-	 * @return void
-	 */
-	function bizboost_support() {
+    /**
+     * Sets up theme defaults and registers support for various WordPress features.
+     *
+     * @since 1.0
+     *
+     * @return void
+     */
+    function bizboost_support()
+    {
 
-		// Add support for block styles.
-		add_theme_support( 'wp-block-styles' );
+        // Add support for block styles.
+        add_theme_support('wp-block-styles');
 
-		// Enqueue editor styles.
-		add_editor_style( 'style.css' );
-
-	}
+        // Enqueue editor styles.
+        add_editor_style('style.css');
+    }
 
 endif;
 
-add_action( 'after_setup_theme', 'bizboost_support' );
+add_action('after_setup_theme', 'bizboost_support');
 
-if ( ! function_exists( 'bizboost_styles' ) ) :
+if (! function_exists('bizboost_styles')) :
 
-	/**
-	 * Enqueue styles.
-	 *
-	 * @since 1.0
-	 *
-	 * @return void
-	 */
+    /**
+     * Enqueue styles.
+     *
+     * @since 1.0
+     *
+     * @return void
+     */
 
-	function bizboost_styles() {
-		// Enqueue theme stylesheet.
-		wp_enqueue_style(
-			'bizboost-style',
-			get_template_directory_uri() . '/style.css',
-			array(),
-			filemtime( get_theme_file_path( 'style.css' ) )
-		);
-
-		wp_enqueue_script(
-            'bizboost-script',
-            get_theme_file_uri( 'assets/js/custom.js' ),
+    function bizboost_styles()
+    {
+        // Enqueue theme stylesheet.
+        wp_enqueue_style(
+            'bizboost-style',
+            get_template_directory_uri() . '/style.css',
             array(),
-            filemtime( get_theme_file_path( 'assets/js/custom.js' ) ),
+            filemtime(get_theme_file_path('style.css'))
+        );
+
+        wp_enqueue_script(
+            'bizboost-script',
+            get_theme_file_uri('assets/js/custom.js'),
+            array(),
+            filemtime(get_theme_file_path('assets/js/custom.js')),
             true
         );
-	}
+    }
 
 endif;
 
-add_action( 'wp_enqueue_scripts', 'bizboost_styles' );
+add_action('wp_enqueue_scripts', 'bizboost_styles');
 
-if ( ! function_exists( 'bizboost_block_editor_styles' ) ) :
+if (! function_exists('bizboost_block_editor_styles')) :
 
     /**
      * Enqueue rtl editor styles
      */
 
-     function bizboost_block_editor_styles() {
-        if( is_rtl() ){
+    function bizboost_block_editor_styles()
+    {
+        if (is_rtl()) {
             wp_enqueue_style(
                 'bizboost-rtl',
-                get_theme_file_uri( 'rtl.css' ),
+                get_theme_file_uri('rtl.css'),
                 array(),
-                filemtime( get_theme_file_path( 'rtl.css' ) )
+                filemtime(get_theme_file_path('rtl.css'))
             );
         }
     }
 
 endif;
 
-add_action( 'enqueue_block_editor_assets', 'bizboost_block_editor_styles' );
+add_action('enqueue_block_editor_assets', 'bizboost_block_editor_styles');
 
 // Add block patterns
 require get_template_directory() . '/inc/block-patterns.php';
